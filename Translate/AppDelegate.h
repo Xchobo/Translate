4f728fceede09c11129778c33d01962e886373cf
Xchobo <xchobo@gmail.com>
2014-04-28 23:31:41 +0800

    Initial Commit

---
+++ Translate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Translate
+//
+//  Created by Xchobo on 2014/4/28.
+//  Copyright (c) 2014年 Xchobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
